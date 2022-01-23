71f012b4df98a74a4295867e23a83c08ac5c54f8
bnemetchek2 <45670468+bnemetchek2@users.noreply.github.com>
2022-01-23 12:56:13 -0700

    add MAUI project

---
+++ OFC_Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OFC_Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OFC_Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OFC_Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
